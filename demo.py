24acac134aea6be496ef8600b252cfcb4044e167
Aurélien Demilly <demilly.aurelien@gmail.com>
2017-04-18 11:33:33 +0200

    refactor and document demo

--- demo.py
+++ demo.py
@@ -1,30 +1,34 @@
+"""Demo module for categorical_encoder package
+"""
 from sklearn import cross_validation, linear_model
 
 import categorical_encoder
 from loader import loader, downloader
 
 
-def study(X, y, mask=None):
-    """Produces scores for different encoding"""
+def study(data, target, mask=None):
+    """Produces scores for different encoding
+    """
 
-    ordinal_scores = scores_for(X, y, 'ordinal', mask)
-    binary_scores = scores_for(X, y, 'binary', mask)
+    ordinal_scores = scores_for(data, target, 'ordinal', mask)
+    binary_scores = scores_for(data, target, 'binary', mask)
 
     return ordinal_scores, binary_scores
 
 
-def scores_for(X, y, encoding_type, mask=None):
-    """Get scores for the data X, y with the encoding encoding_type"""
+def scores_for(data, target, encoding_type, mask=None):
+    """Get scores for the couple data, target with encoding_type
+    """
 
-    eSvc = categorical_encoder.EncoderService(
+    encoding_svc = categorical_encoder.EncoderService(
         encoder_type=encoding_type,
         value_mask=mask
     )
-    X = eSvc.fit_transform(X)
+    data = encoding_svc.fit_transform(data)
 
     clf = linear_model.LogisticRegression()
     scores = cross_validation.cross_val_score(
-        clf, X, y, cv=10
+        clf, data, target, cv=10
     )
 
     return scores
@@ -39,26 +43,27 @@ def print_accuracy(scores, text='', terminator='\n'):
     ) + terminator
     print the_str
 
+
+def run(data_source):
+    """run study for data_source
+    """
+
+    data, target, mask = loader.get_car_data(data_source)
+    ordinal, binary = study(data, target, mask)
+
+    print_accuracy(ordinal, text='Car data, ordinal')
+    print_accuracy(binary, text='Car data, binary')
+
+
 if __name__ == '__main__':
-    """Quick encoding tests on multiple categorical datasets"""
+    """Quick encoding tests on multiple categorical datasets
+    """
 
     downloader.download_car_data()
-
-    X, y, mask = loader.get_car_data('data/cars/car.data.txt')
-    o_scores, b_scores = study(X, y, mask)
-    print_accuracy(o_scores, text='Car data, ordinal')
-    print_accuracy(b_scores, text='Car data, binary')
+    run('data/cars/car.data.txt')
 
     downloader.download_mushroom_data()
-
-    X, y = loader.get_mushroom_data('data/mushrooms/agaricus-lepiota.data.txt')
-    o_scores, b_scores = study(X, y)
-    print_accuracy(o_scores, text='Mushroom data, ordinal')
-    print_accuracy(b_scores, text='Mushroom data, binary')
+    run('data/mushrooms/agaricus-lepiota.data.txt')
 
     downloader.download_splice_data()
-
-    X, y = loader.get_splice_data('data/splice/splice.data.txt')
-    o_scores, b_scores = study(X, y)
-    print_accuracy(o_scores, text='Splice data, ordinal')
-    print_accuracy(b_scores, text='Splice data, binary')
+    run('data/splice/splice.data.txt')
